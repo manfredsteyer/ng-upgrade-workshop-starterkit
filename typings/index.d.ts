8bc0badf8998cd7696fc34946296d2a2ce71b8fb
ManfredSteyer <manfred.steyer@gmx.net>
2016-10-15 21:12:49 +0200

    typings

---
+++ typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/angular-mocks/index.d.ts" />
+/// <reference path="globals/angular-ui-router/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
